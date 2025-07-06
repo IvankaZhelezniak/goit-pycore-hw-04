b8649a1aa5f3298ac084d7f3edd7df1fd4c0da01
Ivanka Zhelezniak <ivankaz685@gmail.com>
2025-07-06 23:44:39 +0200

    Додає зміну

--- task_2/processing.py
+++ task_2/processing.py
@@ -2,7 +2,6 @@ def get_cats_info(path: str) -> list[dict]:
     cats = []
     try:
         with open(path, "r", encoding="utf-8") as file:
-            cat_info = {}
             for line in file:
                 parts = line.strip().split(",")
                 if len(parts) == 3:
